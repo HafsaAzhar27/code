2fc45bdfb0d1ea9c1b4d1839f253f02e09487539
HAFSA <hafsa.strategist@gmail.com>
2024-09-20 00:32:00 -0700

    control

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Link from 'next/link'
 const Footer = () => {
   return (
-    <div className='fixed bottom-0 left-0 right-0 bg-slate-300 flex justify-center p-4 text-slate-800 font-bold '>
+    <div className='fixed bottom-0 left-0 right-0 bg-slate-400 flex justify-center p-4 text-slate-800 font-bold '>
       <Link href="\" className='pl-3 pr-3'>|privacy|</Link>
       <Link href="\"className='pl-3 pr-3'>|terms|</Link>
       <Link href="\"className='pl-3 pr-3'>|conditions|</Link>
