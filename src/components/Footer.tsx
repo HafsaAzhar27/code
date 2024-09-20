7e6db9e2f6781813338957fbe90e189e34df1631
HAFSA <hafsa.strategist@gmail.com>
2024-09-20 00:25:59 -0700

    upload

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'next/link'
+const Footer = () => {
+  return (
+    <div className='fixed bottom-0 left-0 right-0 bg-slate-300 flex justify-center p-4 text-slate-800 font-bold '>
+      <Link href="\" className='pl-3 pr-3'>|privacy|</Link>
+      <Link href="\"className='pl-3 pr-3'>|terms|</Link>
+      <Link href="\"className='pl-3 pr-3'>|conditions|</Link>
+    </div>
+  )
+}
+
+export default Footer
